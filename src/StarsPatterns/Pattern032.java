520970c43c32f8d39149ebbfbff3072032be669f
Shant Khayalian <noiroxc@gmail.com>
2019-10-07 20:06:21 +0400

    Update Pattern032.java

--- src/StarsPatterns/Pattern032.java
+++ src/StarsPatterns/Pattern032.java
@@ -15,16 +15,20 @@ public class Pattern032 {
         System.out.println("## Printing the pattern ##");
 
         // Print i number of stars
-        for (int i = 1; i <= (rows * 2 - 1); i++) {
+        last : for (int i = 1; i <= (rows * 2 - 1); i++) {
             for (int j = 1; j <= rows; j++) {
+                if (i > (rows )){
+                    break last;
+                }
                 if (j == i || j == rows - i + 1) {
                     System.out.print("*");
                 }
                 System.out.print(" ");
+
             }
             System.out.println();
         }
-        scanner.close();
+         scanner.close();
     }
 }
 
