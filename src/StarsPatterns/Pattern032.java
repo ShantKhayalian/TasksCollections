cc5df6bc1d9ba94c677ea20e76d706e90261c975
BeautyIt <noiroxc@gmail.com>
2019-12-11 11:35:06 +0400

    commit new files

--- src/StarsPatterns/Pattern032.java
+++ src/StarsPatterns/Pattern032.java
@@ -5,22 +5,12 @@ import java.util.Scanner;
 public class Pattern032 {
 
     public static void main(String[] args) {
-        // Create a new Scanner object
-        Scanner scanner = new Scanner(System.in);
-
-        // Get the number of rows from the user
-        System.out.println("Enter the number of rows needed to in the pattern ");
-
-        int rows = scanner.nextInt();
-        System.out.println("## Printing the pattern ##");
-
-        // Print i number of stars
-        last : for (int i = 1; i <= (rows * 2 - 1); i++) {
-            for (int j = 1; j <= rows; j++) {
-                if (i > (rows )){
+        last : for (int i = 1; i <= (5 * 2 - 1); i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i > (5 )){
                     break last;
                 }
-                if (j == i || j == rows - i + 1) {
+                if (j == i || j == 5 - i + 1) {
                     System.out.print("*");
                 }
                 System.out.print(" ");
@@ -28,7 +18,6 @@ public class Pattern032 {
             }
             System.out.println();
         }
-         scanner.close();
     }
 }
 
