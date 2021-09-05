2e722c0161b1a358289b72b31c8beab37fec58c3
Shant Khayalian <noiroxc@gmail.com>
2021-09-05 12:33:29 +0400

    Update Pattern001.java

--- src/StarsPatterns/Pattern001.java
+++ src/StarsPatterns/Pattern001.java
@@ -1,9 +1,9 @@
 package StarsPatterns;
 
-public class Pattern001 {
+public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
