40a5c7ec242e75b1c5b960be3547fac5103bd26f
Shant Khayalian <noiroxc@gmail.com>
2021-11-08 21:17:45 +0400

    Update Pattern022.java

--- src/StarsPatterns/Pattern022.java
+++ src/StarsPatterns/Pattern022.java
@@ -2,34 +2,26 @@ package StarsPatterns;
 
 public class Pattern022 {
     public static void main(String[] args) {
-        for (int i=1; i<=5; i++)
-        {
-            for (int j=1; j<=i; j++)
-            {
-                if( j == 1 || j == i )
+       for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i+1; j++) {
+
+                if (j == 0 || j == i || i == 5) {
                     System.out.print("*");
-                else
+                }else {
                     System.out.print(" ");
+                }
             }
             System.out.println();
         }
+        for (int i = 5; i >= 0 ; i--) {
+            for (int j = 0; j < i+1; j++) {
 
-        for (int i=1; i<=5-1; i++)
-        {
-
-            for (int j = 5-1; j >= i; j--)
-            {
-                if( j == 5-1 || j == i || i == 5)
+                if (j == 0 || j == i || j == 5) {
                     System.out.print("*");
-                else
+                }else {
                     System.out.print(" ");
+                }
             }
-
-            for (int k = 1; k < i; k++)
-            {
-                System.out.print(" ");
-            }
-
             System.out.println();
         }
     }
